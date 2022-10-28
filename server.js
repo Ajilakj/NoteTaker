cfffd06495ed48182e8de9ea7df84fbc726e5887
AKJ <kjajila@gmail.com>
2022-10-28 18:11:34 -0400

    add wildward route

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const path = require('path');
-
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
@@ -20,6 +22,61 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
+// GET Route for retrieving all saved notes
+app.get('/api/notes', (req, res) => {
+  console.info(`${req.method} request received for saved notes`);
+
+  fs.readFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+const readAndAppend = (content, file) => {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+//POST Route for saving notes
+app.post('/api/notes', (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to save notes`);
+  //console.dir(req.body);
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+console.log(title);
+  //If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text
+      //feedback_id: uuid(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    res.json(response);
+  } else {
+    res.json('Error in saving new notes');
+  }
+});
+
+app.get('/*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
